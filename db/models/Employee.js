fdbb5ffe562d0803204d5cea3a8433463f3c1017
Jorge Britos <britosj30@gmail.com>
2025-01-26 13:24:10 -0300

    ceación de modelos

---
+++ db/models/Employee.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose'
+
+//FUNCIONARIOS
+const employeeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    num_employee: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    estado: {
+        type: String,
+        enum: ["Active", "Inactive", "On Patrol"],
+        default: "Inactive"
+    },
+    tel: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    entry_date: {
+        type: Date,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    rol: {
+        type: String,
+        enum: ["admin", "employee"],
+        required: true
+    }
+
+}, { collection: 'Employees' })
+
+export default mongoose.model('Employee', employeeSchema)
