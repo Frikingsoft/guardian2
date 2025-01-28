97b7bd69ad1565548513dbdd29587e0071442e78
Frikingsoft <nicolas.barone1@gmail.com>
2025-01-27 21:27:19 -0300

    conexionapi

--- back/db/router/routes/index.js
+++ back/db/router/routes/index.js
@@ -5,10 +5,12 @@ import localRoutes from "./localRoutes.js"
 import outgoRoutes from "./outgoRoutes.js"
 import scheduleRoutes from "./scheduleRoutes.js"
 import  loginRoutes from "./loginRoutes.js"
+import registroRoutes from "./registroRoutes.js"
 const routes = express.Router();
 
 routes.use("/employees", employeeRoutes)
 routes.use("/login", loginRoutes)
+routes.use("/registro",registroRoutes)
 routes.use("/incidents", incidentRoutes)
 routes.use("/locals", localRoutes)
 routes.use("/outgoings", outgoRoutes)
--- back/db/router/routes/registroRoutes.js
+++ back/db/router/routes/registroRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { post} from '../controllers/localController.js';
 import { registro } from "../controllers/registroController.js"
-const localRouter = express.Router();
+const registroRoutes = express.Router();
 
-localRouter.post("/registro", registro , post);
+registroRoutes.post("/registro", registro,post);
 
 
-export default localRouter;
+export default registroRoutes;
